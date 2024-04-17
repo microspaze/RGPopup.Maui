aeed320b62683293074a7741f588a962fc8761fb
microspaze <microspaze@qq.com>
2024-04-17 16:34:16 +0800

    Add test case demo for Intermediate Activity issue #10 fix verifying.

--- RGPopup.Samples/MainPage.xaml.cs
+++ RGPopup.Samples/MainPage.xaml.cs
@@ -90,5 +90,24 @@ namespace RGPopup.Samples
 
             await PopupNavigation.Instance.PushAsync(page);
         }
+
+        private async void OnOpenCamera(object sender, EventArgs e)
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult? photo = await MediaPicker.Default.CapturePhotoAsync();
+                if (photo != null)
+                {
+                    // save the file into local storage
+                    string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+                    using Stream sourceStream = await photo.OpenReadAsync();
+                    using FileStream localFileStream = File.OpenWrite(localFilePath);
+
+                    await sourceStream.CopyToAsync(localFileStream);
+
+                    await PopupNavigation.Instance.PushAsync(_loginPopup);
+                }
+            }
+        }
     }
 }
