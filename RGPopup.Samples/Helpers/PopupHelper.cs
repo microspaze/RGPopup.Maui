62f502e09b7209ac5f1d54c03290ed715bf27108
microspaze <microspaze@qq.com>
2024-05-08 11:07:33 +0800

    Add Obsolete warning for sample's PopupHelper.ShowPopup method. THIS HELPER METHOD IS ONLY FOR DEMO PURPOSE, MAY HAVE COMPATIBILITY ISSUE

--- RGPopup.Samples/Helpers/PopupHelper.cs
+++ RGPopup.Samples/Helpers/PopupHelper.cs
@@ -9,6 +9,7 @@ namespace RGPopup.Samples.Helpers
 {
     public class PopupHelper
     {
+        [Obsolete("THIS HELPER METHOD IS ONLY FOR DEMO PURPOSE, MAY HAVE COMPATIBILITY ISSUE")]
         public static async Task ShowPopup(ContentView content, string title = "")
         {
             var popup = new BasePopupPage(content, title);
